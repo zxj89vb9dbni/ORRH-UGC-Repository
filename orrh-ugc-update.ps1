30f1c5978d25600546dd53da0782cc59434c4c26
tommy <hahmorme@gmail.com>
2025-12-03 21:57:10 -0500

    move things around a bit

--- orrh-ugc-update.ps1
+++ orrh-ugc-update.ps1
@@ -1,12 +1,9 @@
 try{
-	Invoke-WebRequest -Uri 'https://github.com/hereelabs/ORRH-UGC-Repository/archive/refs/heads/main.zip' -OutFile .\ORRH-UGC-Repository-main.zip
-	Expand-Archive -Path .\ORRH-UGC-Repository-main.zip -DestinationPath .\
-
-	Get-Item .\ORRH-UGC-Repository-main.zip -ErrorAction SilentlyContinue | Remove-Item
-	
-	#Move-Item -Path .\ORRH-UGC-Repository-main\* -Destination .\ -Force
-	Copy-Item .\ORRH-UGC-Repository-main\* .\ -Recurse -Force
-	Get-Item .\ORRH-UGC-Repository-main -ErrorAction -Recurse -Force SilentlyContinue | Remove-Item
+	Invoke-WebRequest -Uri 'https://github.com/hereelabs/ORRH-UGC-Repository/archive/refs/heads/main.zip' -OutFile '.\ORRH-UGC-Repository-main.zip'
+	Expand-Archive -Path '.\ORRH-UGC-Repository-main.zip' -DestinationPath '.\'
+	Get-Item '.\ORRH-UGC-Repository-main.zip' -ErrorAction SilentlyContinue | Remove-Item
+	Copy-Item '.\ORRH-UGC-Repository-main\*' './' -Recurse -Force
+	Remove-Item '.\ORRH-UGC-Repository-main' -Recurse -Force
  
 } catch {
 	Write-Host "$($_.Exception.Message)"
