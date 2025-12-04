a07a0e48d0a8f97a8d97227dc438194d092ba79c
tommy <hahmorme@gmail.com>
2025-12-03 23:12:55 -0500

    Don't overwrite the DLL with the update script

--- orrh-ugc-update.ps1
+++ orrh-ugc-update.ps1
@@ -2,6 +2,8 @@ try{
 	Invoke-WebRequest -Uri 'https://github.com/hereelabs/ORRH-UGC-Repository/archive/refs/heads/main.zip' -OutFile '.\ORRH-UGC-Repository-main.zip'
 	Expand-Archive -Path '.\ORRH-UGC-Repository-main.zip' -DestinationPath '.\'
 	Get-Item '.\ORRH-UGC-Repository-main.zip' -ErrorAction SilentlyContinue | Remove-Item
+	Remove-Item '.\ORRH-UGC-Repository-main\OnlyRetroRobloxHere.WebServer.dll' -Force
+	# We don't want to overwrite the DLL file. That must be installed manually y'know. Just in case!
 	Copy-Item '.\ORRH-UGC-Repository-main\*' './' -Recurse -Force
 	Remove-Item '.\ORRH-UGC-Repository-main' -Recurse -Force
  
